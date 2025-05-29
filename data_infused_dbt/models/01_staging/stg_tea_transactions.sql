f3fc8d8a7be792949c279f9283b7c75c5e1a9f58
YinVang <hoyinvang@gmail.com>
2025-05-29 17:26:29 +0700

    t1

--- data_infused_dbt/models/01_staging/stg_tea_transactions.sql
+++ data_infused_dbt/models/01_staging/stg_tea_transactions.sql
@@ -4,21 +4,21 @@
 ) }}
 
 with source as (
-    select * from {{ source('tea_schema', 'tea_transactions') }}
+  select * from {{ source('tea_schema', 'tea_transactions') }}
 ),
 
 renamed as (
-    select
-        transaction_id as TRANSACTION_ID,
-        tea_id::int as TEA_ID,
-        customer_id as CUSTOMER_ID,
-        quantity::int as QUANTITY,
-        price::float as PRICE,
-        name as TEA_NAME,
-        category as CATEGORY,
-        country as COUNTRY,
-        to_timestamp(timestamp) as TRANSACTION_TIME
-    from source
+  select
+    transaction_id,
+    tea_id::int as tea_id,
+    customer_id,
+    quantity::int as quantity,
+    price::float as price,
+    name as tea_name,
+    category,
+    country,
+    to_timestamp(timestamp) as transaction_time
+  from source
 )
 
 select * from renamed
